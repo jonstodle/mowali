6ecf4e6a2ab1886a1325b744a2453fac4f8e63b7
Jon Stødle <jonstodle@outlook.com>
2016-01-10 22:51:39 +0100

    Random stuff

---
+++ Mowali/Controls/MoviePosterItem.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Mowali.Controls
+{
+	public sealed partial class MoviePosterItem : UserControl
+	{
+		public MoviePosterItem()
+		{
+			this.InitializeComponent();
+			this.RegisterPropertyChangedCallback(UserControl.WidthProperty, WidthProperty_Changed);
+			this.DataContextChanged += MoviePosterItem_DataContextChanged;
+		}
+
+		private void WidthProperty_Changed(DependencyObject sender, DependencyProperty dp)
+		{
+			Height = Width * 1.5;
+		}
+
+		private void MoviePosterItem_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+		{
+			
+		}
+	}
+}
